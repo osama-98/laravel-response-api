b63e75be0a496486c0a118a5685e37d45cff56d0
Osama Sadah <osama.sada98@gmail.com>
2026-01-10 11:54:54 +0300

    feat: pagination types and upgrade php version to 8.1

--- src/ApiResponse.php
+++ src/ApiResponse.php
@@ -3,11 +3,10 @@
 namespace Osama\ApiResponse;
 
 use Illuminate\Contracts\Pagination\CursorPaginator;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Pagination\CursorPaginator as ConcreteCursorPaginator;
-use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Pagination\Paginator as ConcretePaginator;
 use Illuminate\Support\Arr;
 use InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Response;
@@ -49,19 +48,20 @@ final class ApiResponse
     /**
      * Prepare paginated data response.
      *
-     * @param  CursorPaginator<int, mixed>|Paginator<int, mixed>  $paginated
+     * @param CursorPaginator<int, mixed>|Paginator<int, mixed>|LengthAwarePaginator<int, mixed> $paginated
+     * @param array<string, mixed>|null $meta
      */
     public static function pagination(
-        CursorPaginator|Paginator $paginated,
-        callable|string|null $mapper = null,
-        array|null $meta = []
-    ): JsonResponse {
-        /** @var LengthAwarePaginator<int, mixed>|ConcreteCursorPaginator<int, mixed>|ConcretePaginator<int, mixed> $paginated */
+        CursorPaginator|Paginator|LengthAwarePaginator $paginated,
+        callable|string|null                           $mapper = null,
+        array|null                                     $meta = []
+    ): JsonResponse
+    {
         $items = $paginated->items();
 
         if ($mapper !== null) {
             if (is_callable($mapper)) {
-                $items = array_map(fn ($item) => $mapper($item), $items);
+                $items = array_map(fn($item) => $mapper($item), $items);
             } elseif (is_string($mapper) && class_exists($mapper) && method_exists($mapper, 'collection')) {
                 $items = $mapper::collection($items);
             } else {
@@ -69,10 +69,11 @@ final class ApiResponse
             }
         }
 
+        /** @var Arrayable<string, mixed> $paginated */
         return self::builder()
             ->data([
                 'data' => $items,
-                'pagination' => Arr::except($paginated->jsonSerialize(), ['data']),
+                'pagination' => Arr::except($paginated->toArray(), ['data']),
                 ...(! empty($meta) ? ['meta' => $meta] : []),
             ])
             ->send();
@@ -82,11 +83,12 @@ final class ApiResponse
      * Successfully created response handler
      */
     public static function created(
-        mixed $data = null,
+        mixed   $data = null,
         ?string $resource = null,
         ?string $message = null,
-        int $status = Response::HTTP_CREATED
-    ): JsonResponse {
+        int     $status = Response::HTTP_CREATED
+    ): JsonResponse
+    {
         /** @var string|null $message */
         $message = $message ?: __('api-response::messages.resource_created', ['resource' => $resource]);
 
@@ -101,11 +103,12 @@ final class ApiResponse
      * Successfully updated response handler
      */
     public static function updated(
-        mixed $data,
+        mixed   $data,
         ?string $resource = null,
         ?string $message = null,
-        int $status = Response::HTTP_OK
-    ): JsonResponse {
+        int     $status = Response::HTTP_OK
+    ): JsonResponse
+    {
         /** @var string|null $message */
         $message = $message ?: __('api-response::messages.resource_updated', ['resource' => $resource]);
 
@@ -120,12 +123,13 @@ final class ApiResponse
      * Destroy response handler
      */
     public static function destroyed(
-        bool $destroyed,
+        bool    $destroyed,
         ?string $resource = null,
         ?string $message = null,
-        int $successCode = Response::HTTP_OK,
-        int $errorCode = Response::HTTP_BAD_REQUEST
-    ): JsonResponse {
+        int     $successCode = Response::HTTP_OK,
+        int     $errorCode = Response::HTTP_BAD_REQUEST
+    ): JsonResponse
+    {
         if ($destroyed) {
             /** @var string $deletedMessage */
             $deletedMessage = __('api-response::messages.resource_deleted', ['resource' => $resource]);
